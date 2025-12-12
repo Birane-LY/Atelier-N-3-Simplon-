bb7ba2ceece959bf682165a885a55669ed3172f3
Fatoumata DRAME <dramefatoumata029@gmail.com>
2025-12-12 10:51:10 +0000

    J'ai ajouter un autre fichier python

---
+++ projet_1/Exercice_3.py
@@ -0,0 +1,26 @@
+#Demandez à l'utilisateur de saisir 10 nombres (positifs ou négatifs).
+#Créez une nouvelle liste qui contient uniquement les nombres supérieurs ou égaux à 0
+#(nombres non négatifs).Affichez la nouvelle liste.
+
+
+List = []
+
+List_1 = []
+
+for i in range(1,11) :
+
+    nombre = float(input("Veuillez entrer les nombres"))
+
+    List.append(nombre)
+    if nombre >= 0 :
+
+      List_1.append(nombre)
+
+    else :
+       print("Le nombre est negatif") 
+
+print(f"la lsite des nombres positifs est:{List_1}")
+print(f"La liste des dix nombres est:{List}")
+
+
+
